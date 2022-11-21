ba8248040c91be97bed06f2c142db5f937e35239
H.K <16552133+bj1024@users.noreply.github.com>
2022-11-21 09:45:33 +0900

    add heredoc.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 console.log('hello')
+
+
+// ヒアドキュメント
+let str = `
+ヒアドキュメント（別の呼び方としてヒア文字列、heredocなど）は、
+文字列リテラルを、シェルスクリプトやプログラミング言語のソースコード中に埋め込むための1つの方法である。
+
+ヒアドキュメント - Wikipedia
+https://ja.wikipedia.org/wiki/%E3%83%92%E3%82%A2%E3%83%89%E3%82%AD%E3%83%A5%E3%83%A1%E3%83%B3%E3%83%88
+`
+console.log(str)
