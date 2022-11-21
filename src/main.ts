62860a351aafd998a3ce0d1ba5db788b0a55c5ea
H.K <16552133+bj1024@users.noreply.github.com>
2022-11-21 15:57:33 +0900

    Function.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import {parse} from "csv-parse";
 import * as fs from 'fs';
+// import iconv from "iconv-lite";
 // import * as readline from "readline"
 
 // ヒアドキュメント
@@ -10,87 +11,71 @@ let str = `
 ヒアドキュメント - Wikipedia
 https://ja.wikipedia.org/wiki/%E3%83%92%E3%82%A2%E3%83%89%E3%82%AD%E3%83%A5%E3%83%A1%E3%83%B3%E3%83%88
 `
-console.log(str)
+console.debug(`heredoc=[${str}]`)
 console.debug(`pwd=[${process.cwd()}]`);
 
 
-const iconv = require('iconv-lite');
-
-try {
-
-
-  const fname = `./data/data_sjis_excelexport.csv`
-
-  // read entire file.
-  fs.readFile(fname, 'utf8', (err, data) => {
+const readFileAll = (infname: string) => {
+  // read entire file.(test)
+  fs.readFile(infname, 'utf8', (err, data) => {
     if (err) {
       console.error(`file read error.[${err}]`)
       return
     }
-    console.error(`file contents. read length=[${data.length}]`)
+    console.error(`readFileAll.read length=[${data.length}]`)
   })
-
-  // read stream
-  const parser = parse({delimiter: ',', columns: true});
-  const stream = fs.createReadStream(fname)
-      .pipe(iconv.decodeStream("Shift_JIS"))  // pipeにwriteableを指定する。
-      .pipe(parser) ///
-    // .pipe(writer)
-  ;
-
-  //
-  const records: any = []
-  parser.on('readable', () => {
-    let record;
-    while ((record = parser.read()) !== null) {
-      record['no'] = parseInt(record['no'])
-      // console.debug(record['no'],typeof record['no'])
-      // console.debug(record)
-
-      for (let [key, value] of Object.entries(record)) {
-        console.debug('key:' + key + ' value:' + value, typeof value);
-      }
-      records.push(record);
-    }
-  });
-  parser.on('close', () => {
-
-    console.debug('on close', records);
-
-    const jsonstr = JSON.stringify(records, null, 2)
-    console.debug('jsonstr = ', jsonstr);
-
-    // 書き込み
-    fs.writeFile("./out/data.json", jsonstr, (err) => {
-      if (err) throw err;
-      console.log('succeed.');
-    });
-  })
-
-  //
-  // var reader = readline.createInterface({input: stream});
-  // reader
-  //   .on("line", (line: string) => {
-  //       // console.debug(`[${line}]`)
-  //       writer.write(line + "\r\n");
-  //
-  //     //
-  //     //   console.debug(`records=[${records}]`)
-  //     // for (const record of records) {
-  //     //   console.log(record);
-  //     // }
-  //
-  //     })
-  //
-  //   .on('close', () => {
-  //     console.log('Have a great day!');
-  //     writer.end();
-  //     process.exit(0);
-  //   });
-
-} catch (e) {
-  console.error(`erorr.[${e}]`)
-} finally {
-
 }
 
+const csvToJson = (infname: string, outfname: string) => {
+  console.log(`csvToJson infname=[${infname}] out=[${outfname}]`)
+
+  try {
+    const iconv = require("iconv-lite")
+
+    // read stream
+    const parser = parse({delimiter: ',', columns: true});
+    const stream = fs.createReadStream(infname)
+      .pipe(iconv.decodeStream("Shift_JIS"))  // pipeにwriteableを指定する。
+      .pipe(parser) // csv-parse
+    //
+    const records: any = []
+    parser.on('readable', () => {
+      let record;
+      while ((record = parser.read()) !== null) {
+        record['no'] = parseInt(record['no']) // convert type test.
+        // console.debug(record['no'],typeof record['no'])
+        // console.debug(record)
+
+        for (let [key, value] of Object.entries(record)) {
+          // console.debug('key:' + key + ' value:' + value, typeof value);
+        }
+        records.push(record);
+      }
+    });
+    parser.on('close', () => {
+      // console.debug('on close', records);
+
+      const jsonstr = JSON.stringify(records, null, 2)
+      // console.debug('jsonstr = ', jsonstr);
+
+      // 書き込み
+      fs.writeFile(outfname, jsonstr, (err) => {
+        if (err) throw err;
+        console.log('succeed.');
+      });
+    })
+
+  } catch (e) {
+    console.error(`erorr.[${e}]`)
+  } finally {
+
+  }
+}
+
+const input_file = `./data/data_sjis_excelexport.csv`
+const output_file = `./out/data_excelexport.json`
+readFileAll(input_file)
+
+console.debug("csvToJson ---- start.")
+csvToJson(input_file, output_file)
+console.debug("csvToJson ---- end.")  // 非同期なのでcsvToJsonはすぐに終わるので注意。
